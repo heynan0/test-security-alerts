75ea7b723e6a1e822f6fa4daeb7327d5207ff64e
Renan Martins <heynan0@github.com>
2020-01-28 11:51:35 -0800

    Add sample code

---
+++ index.js
@@ -0,0 +1,36 @@
+const crypto = require('crypto-js');
+const fs = require('fs');
+const moment = require('moment');
+const path = require('path');
+const rimraf = require('rimraf');
+
+function testMoment() {
+    const today = moment();
+
+    console.log(today.format("YYYY-MM-DD"));
+}
+
+testMoment();
+
+function testCrypto() {
+    const phrase = "Hello World!";
+
+    const md5 = crypto.MD5(phrase);
+
+    console.log({ md5 });
+}
+
+testCrypto();
+
+function testRimraf() {
+    const filePath = path.join(__dirname, 'hello-world.txt')
+    const content = 'This is hello world';
+
+    fs.writeFileSync(filePath, content);
+
+    setTimeout(() => {
+        rimraf.sync(filePath);
+    }, 10000);
+}
+
+testRimraf();
